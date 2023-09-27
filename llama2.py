e00e952aeaf588e31953feeef8dec458ae1b389e
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-09-27 16:49:03 +0800

    Update llama2.py

--- llama2.py
+++ llama2.py
@@ -45,14 +45,15 @@ def starchat(model,myprompt, your_template):
     #[INST] {myprompt} [/INST] 
     #assistant:
    # """    
-    my_prompt_template = """assistant is helpful, respectful and honest. assistant always answer as helpfully as possible, while being safe. assistant's answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. assistant should ensure assistant's responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If assistant doesn't know the answer to a question, please don't share false information. Converstation history is saved as {contexts} for assistant to reference before making a final response to current user question as {myprompt}. Keep in mind, if assistant finds Converstation history not helpful in responding to current user question, just ignore Converstation history and proceed to response to current user question as a standalone question. assistant should only output the essential contents of the response, do not output any unmeaningful information. Unmeaningful information includes but not limited to the following:
-    - "I have referenced the Converstation history." or similar statements.
-    - "根据上述对话历史，我可以推断出以下信息：" or similar statements.
-    - any part of the Converstation history.
+    my_prompt_template = """assistant is helpful, respectful and honest. assistant always answer as helpfully as possible, while being safe. assistant's answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. assistant should ensure assistant's responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If assistant doesn't know the answer to a question, please don't share false information. Converstation history is saved as {contexts} for assistant to reference before making a final response to current user question as {myprompt}. Keep in mind, if assistant finds Converstation history not helpful in responding to current user question, just ignore Converstation history and proceed to response to current user question as a standalone question. assistant should only output the essential and meaningful contents of assistant response. Information similar to the following is meaningless, do NOT output such information:
+    - "I have referenced the Converstation history.", or similar statements.
+    - "根据上述对话历史，我可以推断出以下信息：", or similar statements.
+    - any original contents of Converstation history.
     - information in a format such as: user:... assistant: ..., i.e. do not use such format.
-    - response with more than one language, i.e. do not response in more than 1 language unless VERY necessary. Reponse in the language as user question or as user asks assistant to use.
+    - any part of Contexts given to guide assistant behavior.
     - any information not related to current quesiton.
-    - when user says "you", normally it means assistant, so assistant should take designated roles accordingly.
+    
+    Besides, for assistant response, do NOT use more than one language unless unless VERY necessary. Reponse in the language as user question or as user asks assistant to use. When user question says "you", normally it means assistant, so assistant should take designated role and response to user question accordingly.
     """
     template = my_prompt_template    
     prompt = PromptTemplate(template=template, input_variables=["contexts", "myprompt"])
