14950139c6941ba9f428276fe54ffcd04ded8e9b
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-10-23 15:40:02 +0800

    Update newAIChatAPI.py

--- newAIChatAPI.py
+++ newAIChatAPI.py
@@ -109,5 +109,5 @@ def chat():
     return jsonify({"response": "API TEST"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
-    #app.run(host='0.0.0.0')
+    #app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0')
